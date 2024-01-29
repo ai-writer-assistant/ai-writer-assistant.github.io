6a1ddd6e32fdd6ba2c75cb3ff6acd6bae7a5b97d
wagenrace <nijhof.dns@gmail.com>
2024-01-29 20:55:54 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+const input_text = ref<string>("")
+
+</script>
+
+<template>
+  <div class="flex bg-slate-600">
+    <input class="h-screen w-full" type="text" v-model="input_text" />
+    <div class="w-full">
+      <p>
+        {{ input_text }}
+      </p>
+    </div>
+  </div>
+</template>
+
