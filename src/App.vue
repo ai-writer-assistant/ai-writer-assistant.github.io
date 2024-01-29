6ed6340e7164bc0aabf1fa136146ce59a4edda70
wagenrace <nijhof.dns@gmail.com>
2024-01-29 22:10:17 +0100

    add streaming

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <script setup lang="ts">
 import { ref } from "vue"
 
-const input_text = ref<string>("")
+import ollama from 'ollama'
+const input_text = ref<string>("Why is the sky blue?")
+const output_text = ref<string>("")
 
+
+async function sendToModel() {
+  console.log("Send message")
+
+  const response = await ollama.chat({ model: 'llama2', messages: [{ role: 'user', content: input_text.value }], stream: true })
+  for await (const part of response) {
+    output_text.value += part.message.content
+  }
+}
 </script>
 
 <template>
   <div class="flex bg-slate-600">
-    <input class="h-screen w-full" type="text" v-model="input_text" />
+    <div class="w-full h-screen">
+      <button class="bg-green-400 p-3 rounded-xl" @click="sendToModel()">Send</button>
+      <input class="w-full h-full" type="text" v-model="input_text" />
+    </div>
     <div class="w-full">
       <p>
-        {{ input_text }}
+        {{ output_text }}
       </p>
     </div>
   </div>
