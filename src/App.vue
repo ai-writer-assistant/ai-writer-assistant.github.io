e86db18196fd3a0b01471a23be0aa3225a34fb3a
wagenrace <nijhof.dns@gmail.com>
2024-02-02 11:16:38 +0100

    Have multiple input and outputs

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
 import { ref } from "vue"
 
 import ollama from 'ollama'
-const input_text = ref<string>("Why is the sky blue?")
-const output_text = ref<string>("")
 const is_loading = ref<boolean>(false)
 
 interface ResOut {
@@ -28,16 +26,25 @@ all_res_out.value.push({
   message: "Why am I blue?",
   output: "You are not"
 })
-async function sendToModel() {
+async function sendToModel(id: Number) {
+  if (is_loading.value) {
+    return
+  }
   is_loading.value = true
+  const _res_out = all_res_out.value.find((res_out) => res_out.id === id)
+  if (!_res_out) {
+    is_loading.value = false
+    return
+  }
 
+  _res_out.output = ""
   const response = await ollama.chat({
     model: 'blog',
-    messages: [{ role: 'user', content: input_text.value }],
+    messages: [{ role: 'user', content: _res_out.message }],
     stream: true
   })
   for await (const part of response) {
-    output_text.value += part.message.content
+    _res_out.output += part.message.content
   }
   is_loading.value = false
 }
@@ -46,11 +53,11 @@ async function sendToModel() {
 <template>
   <div class="flex h-full">
     <div class="w-full h-full flex flex-col">
-      <button class="bg-green-400 p-3 rounded-xl grow-0 disabled:bg-gray-400" :disabled="is_loading"
-        @click="sendToModel()">Send</button>
       <div class="w-full grow" v-for="res_out in all_res_out" :key="res_out.id">
         <div class="flex h-full">
-          <input class="w-1/2 h-full" type="text" v-model="input_text" />
+          <textarea class="w-1/2 h-full" type="text" v-model="res_out.message" />
+          <button class="bg-green-400 p-3 rounded-xl grow-0 disabled:bg-gray-400" :disabled="is_loading"
+            @click="sendToModel(res_out.id)">Send</button>
           <p class="w-1/2 p-4 h-full">
             {{ res_out.output }}
           </p>
