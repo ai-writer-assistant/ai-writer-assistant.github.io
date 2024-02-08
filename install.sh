a0564db7715057408004cde65aea4fa107abd1af
wagenrace <nijhof.dns@gmail.com>
2024-02-08 13:46:22 +0100

    install latest version of nodejs

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
-# Update package manager
-sudo apt update -y
 
 # Install npm
-sudo apt install npm -y
+curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash - &&\
+sudo apt install -y nodejs
 
 curl https://ollama.ai/install.sh | sh
 
 npm i
 
 ollama create blog -f ./Modelfile
+
+./start.sh
